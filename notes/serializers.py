c7861205827f114c3e702e3ee79347816ed21c62
Doug <doug@localhost.localdomain>
2018-08-17 13:16:00 -0400

    registration

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note, Notebook, Tag
+from django.db import IntegrityError
 
 # For Profile Page
 class UserSerializer(serializers.ModelSerializer):
@@ -21,13 +22,18 @@ class UserCreationSerializer(serializers.ModelSerializer):
 		fields = ['email', 'first_name', 'last_name', 'password']
 
 	def create(self, validated_data):
-		user = User(
-			email = validated_data['email'],
-			first_name = validated_data['first_name'],
-			last_name = validated_data['last_name'],
-		)
-		user.set_password(validated_data['password'])
-		user.save()
+		user = None
+		try:
+			user = User(
+				email = validated_data['email'],
+				username = validated_data['email'],
+				first_name = validated_data['first_name'],
+				last_name = validated_data['last_name'],
+			)
+			user.set_password(validated_data['password'])
+			user.save()
+		except IntegrityError as e:
+			raise serializers.ValidationError(". ".join(e.args))
 		return user
 
 class NotebookSerializer(serializers.ModelSerializer):
